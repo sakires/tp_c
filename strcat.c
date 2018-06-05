8abb889524139d27f8cf64485d1e468317ddea11
sakires <34483208+sakires@users.noreply.github.com>
2018-06-05 15:27:43 +0200

    Add files via upload

--- strcat.c
+++ strcat.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-
+/*
+	my_strcat
+	entrer : deux chaine de caractere donc celle de destination assez grandre pour contenir src 
+	sortie : la chaine concaténé des deux premiere chaine et a l emplacement memoire de dest
+*/
 char *my_strcat(char *dest , const char *src){
+	
+	char *copy = dest;
+
 	while(*dest !='\0'){
 		++dest;
 	}
-	printf("marche");	
 	while(*src !='\0'){
 		*dest = *src;
 		++src;
 		++dest;
 	}
-	*dest = '\0';	
-	return dest;
+	*dest  = '\0';
+	return copy;
 }
 
 
 
 int main(void){
-	printf("mot concat est %s", my_strcat("marche"," stp"));
+	char marche[50] = "marche ";
+	printf("mot concat est %s \n", my_strcat(marche,"stp"));
 	return 0;
 }
